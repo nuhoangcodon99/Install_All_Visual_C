f7f1de66d18493de87e1824b83d67fc462e04fb1
Nuhoang <tranvienvien51@gmail.com>
2024-06-25 02:55:34 +0700

    Delete All_Visual_C++.ps1.ps1

--- All_Visual_C++.ps1.ps1
+++
@@ -1,5 +0,0 @@
-# Cài đặt Visual C++ Redistributable Packages (x64)
-Start-Process -FilePath "vc_redist.x64.exe" -ArgumentList "/install", "/quiet", "/norestart" -Wait
-
-# Cài đặt Visual C++ Redistributable Packages (x86)
-Start-Process -FilePath "vc_redist.x86.exe" -ArgumentList "/install", "/quiet", "/norestart" -Wait
